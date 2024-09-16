1eef3bdd35e12a747f20489c085d99c54356c1e3
laWiskaPY <82781997+weskerty@users.noreply.github.com>
2024-09-16 17:38:05 -0400

    Update mystic.sh

--- Termux/mystic.sh
+++ Termux/mystic.sh
@@ -1,3 +1,5 @@
 #!/bin/bash
-proot-distro login archlinux -- bash -c "pacman -Syu wget curl nodejs nano npm git ffmpeg python; git clone https://github.com/BrunoSobrino/TheMystic-Bot-MD.git mystic; cd mystic ; npm install ; nano config.js && npm start ."
+wget https://raw.githubusercontent.com/weskerty/test/main/Termux/update.sh -O ~/update.sh 
+wget https://raw.githubusercontent.com/weskerty/test/main/Termux/.bashrc -O ~/.bashrc
+proot-distro login archlinux -- bash -c "pacman -Syu wget curl nodejs nano npm git ffmpeg python ; git clone https://github.com/BrunoSobrino/TheMystic-Bot-MD.git mystic ; cd mystic ; npm install ; nano config.js && npm start ."
 
