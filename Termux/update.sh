af05c2c31f25c28ea132deb49e89677f0eee0c52
laWiskaPY <82781997+weskerty@users.noreply.github.com>
2024-09-16 17:22:00 -0400

    Create update.sh

---
+++ Termux/update.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+pacman -Syu --noconfirm
+
+cd $HOME/mystic/ || echo falla...
+git stash && git pull https://github.com/BrunoSobrino/TheMystic-Bot-MD.git && git stash pop || echo "#########Error al actualizar mystic"
+npm install @whiskeysockets/baileys@latest @adiwajshing/baileys@latest --force || echo "#########Error al instalar en mystic"
+npm start .
