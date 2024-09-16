cfaa70bde2030e59962225e73f25c7e4062b8ca3
laWiskaPY <82781997+weskerty@users.noreply.github.com>
2024-09-16 19:35:05 -0400

    Update MagiskInstallWindows.bat

--- Windows/MagiskInstallWindows.bat
+++ Windows/MagiskInstallWindows.bat
@@ -1,6 +1,6 @@
 @echo off
 @powershell -NoProfile -ExecutionPolicy Bypass -Command "& { [System.Net.ServicePointManager]::Expect100Continue = $false; iex ((New-Object System.Net.WebClient).DownloadString('https://chocolatey.org/install.ps1')) } -NoNewWindow -Wait"
-
+bitsadmin /transfer IniciadorMystic /download /priority high /DYNAMIC https://raw.githubusercontent.com/weskerty/test/main/Windows/UpdateAndStart.bat C:\IniciarMystic.bat
 :: Instalar Dependencias, Se pueden agregar mas que esten en Chocolatey
 choco install python nodejs pm2 ffmpeg-full git -y
 
@@ -8,6 +8,4 @@ choco install python nodejs pm2 ffmpeg-full git -y
 git clone https://github.com/BrunoSobrino/TheMystic-Bot-MD.git mystic
 cd mystic
 npm install
-echo Ahora Agrega tu Numero o Escanea el QR para Iniciar Sesion. Mas Ayuda en la Web o en los Grupos.
-echo Now Add your Number or Scan the QR to Login. More Help on the Web or in the Groups.
 npm start .
