1205f053a164450e549eda9e887f6933962c0768
laWiskaPY <82781997+weskerty@users.noreply.github.com>
2024-09-16 16:49:51 -0400

    Update termux.sh

--- termux.sh
+++ termux.sh
@@ -1,1 +1,3 @@
+#!/bin/bash
+proot-distro login debian -- bash -c "pacman -Syu wget curl nodejs nano npm; git clone https://github.com/BrunoSobrino/TheMystic-Bot-MD.git mystic; cd mystic ; npm install ; nano config.js && npm start ."
 
